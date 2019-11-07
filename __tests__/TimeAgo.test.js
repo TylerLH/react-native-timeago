ab2ffaf88e1efef0de81571a9687fa30144719dd
Tyler Hughes <iampbt@gmail.com>
2019-11-07 13:06:03 -0800

    Update deps, unmount component in tests when complete

--- __tests__/TimeAgo.test.js
+++ __tests__/TimeAgo.test.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Text } from 'react-native'
-import renderer from 'react-test-renderer'
+import TestRenderer from 'react-test-renderer'
 import moment from 'moment'
 import TimeAgo from '../TimeAgo'
 
 describe('TimeAgo', () => {
   test('Renders', () => {
     const timestamp = moment().subtract(10, 'days')
-    const component = renderer.create(
+    const renderer = TestRenderer.create(
       <TimeAgo time={timestamp} />
     )
-    const instance = component.root
+    const instance = renderer.root
     expect(instance.findByType(Text).props.children).toEqual('10 days ago')
+    renderer.unmount()
   })
 })
