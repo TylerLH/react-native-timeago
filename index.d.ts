157871a4a7a301c0e64b2d00c5ee1afae3dcd932
Roven Kyle Narra <ellamateogang@gmail.com>
2023-05-16 01:42:58 +0800

    added textstyle

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import {TextStyle} from 'react-native'
 
 export interface Props {
     time: string | number | Array<any> | Date;
     interval?: number;
     hideAgo?: boolean;
+    textStyle?: TextStyle,
 }
 
 declare module 'react-native-timeago' {
